0d359c0f99ff3c5f8e54ff3e2c29500f748106e0
beeth0ven <beeth0vendev@gmail.com>
2020-06-03 13:14:21 +0800

    抽离 PostListTile

--- lib/pages/tagposts/TagPostsPage.dart
+++ lib/pages/tagposts/TagPostsPage.dart
@@ -1,5 +1,4 @@
 
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
@@ -7,11 +6,9 @@ import 'package:tinylearn_client/functional/foundation/SilenceChangeNotifier.dar
 import 'package:tinylearn_client/functional/networking/TagService/TagService.dart';
 import 'package:tinylearn_client/functional/networking/TagService/types/TagPostsInput.dart';
 import 'package:tinylearn_client/models/CursorPosts.dart';
-import 'package:tinylearn_client/models/Post.dart';
 import 'package:tinylearn_client/models/Tag.dart';
+import 'package:tinylearn_client/widgets/PostListTile.dart';
 import 'package:tinylearn_client/widgets/Spinner.dart';
-import 'package:tinylearn_client/functional/foundation/int_time.dart';
-import 'package:tinylearn_client/functional/dateformat/shorttext_time.dart';
 
 class TagPostsPage extends StatefulWidget {
   final String tagName;
@@ -52,14 +49,14 @@ class _TagPostsPageState extends State<TagPostsPage> with AutomaticKeepAliveClie
               return false;
             },
             child: ListView.separated(
-              padding: const EdgeInsets.symmetric(vertical: 16),
+              padding: const EdgeInsets.only(top: 16),
               itemCount: tagPostsNotifier.postsLength + 1,
               itemBuilder: (context, index) {
                 if (index == tagPostsNotifier.postsLength) {
                   return tagPostsNotifier.hasNoMoreData ? Container() : Spinner();
                 }
                 final post = tagPostsNotifier.tag.posts.items[index];
-                return _buildPostListTile(context, post); 
+                return PostListTile(post: post); 
               }, 
               separatorBuilder: (BuildContext context, int index) => Divider(),
             ),
@@ -69,70 +66,6 @@ class _TagPostsPageState extends State<TagPostsPage> with AutomaticKeepAliveClie
     );
   }
 
-
-  Widget _buildPostListTile(BuildContext context, Post post) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(width: 8),
-        _buildAvatar(post),
-        SizedBox(width: 8),
-        Expanded(child: _buildPostBody(post, context),),
-        SizedBox(width: 16),
-      ],
-    );
-  }
-
-  Column _buildPostBody(Post post, BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Row(
-          children: <Widget>[
-            Text('@${post.user?.username ?? ""}', style: Theme.of(context).textTheme.subtitle1.copyWith(fontWeight: FontWeight.bold)),
-            SizedBox(width: 8),
-            Text('${post.created.time.shortText}', style: Theme.of(context).textTheme.subtitle2.copyWith(color: Colors.grey),)
-          ],
-        ),
-        SizedBox(height: 4),
-        Text(post.content, overflow: TextOverflow.ellipsis, maxLines: 5, style: Theme.of(context).textTheme.bodyText1),
-        SizedBox(height: 8),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            _buildIconButton(
-              icon: Icons.thumb_up,
-            ),
-            _buildIconButton(
-              icon: Icons.bookmark,
-            ),
-            _buildIconButton(
-              icon: Icons.more_horiz,
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-
-  CircleAvatar _buildAvatar(Post post) {
-    return CircleAvatar(
-      backgroundImage: CachedNetworkImageProvider(
-        post.user?.imageUrl ?? ""
-      ),
-      backgroundColor: Colors.grey[100],
-    );
-  }
-
-  Widget _buildIconButton({
-    IconData icon,
-    VoidCallback onPressed
-  }) {
-    return GestureDetector(
-        child: Icon(icon, size: 16, color: Colors.grey,),
-        onTap: onPressed,
-      )
-    ;
-  }
 }
 
 class TagPostsNotifier extends SilenceChangeNotifier {
---
+++ lib/widgets/PostListTile.dart
@@ -0,0 +1,79 @@
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:tinylearn_client/models/Post.dart';
+import 'package:tinylearn_client/functional/foundation/int_time.dart';
+import 'package:tinylearn_client/functional/dateformat/shorttext_time.dart';
+
+class PostListTile extends StatelessWidget {
+
+  final Post post;
+
+  const PostListTile({Key key, this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(width: 8),
+        _buildAvatar(post),
+        SizedBox(width: 8),
+        Expanded(child: _buildPostBody(post, context),),
+        SizedBox(width: 16),
+      ],
+    );
+  }
+
+
+  Column _buildPostBody(Post post, BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Text('@${post.user?.username ?? ""}', style: Theme.of(context).textTheme.subtitle1.copyWith(fontWeight: FontWeight.bold)),
+            SizedBox(width: 8),
+            Text('${post.created.time.shortText}', style: Theme.of(context).textTheme.subtitle2.copyWith(color: Colors.grey),)
+          ],
+        ),
+        SizedBox(height: 4),
+        Text(post.content, overflow: TextOverflow.ellipsis, maxLines: 5, style: Theme.of(context).textTheme.bodyText1),
+        SizedBox(height: 8),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            _buildIconButton(
+              icon: Icons.thumb_up,
+            ),
+            _buildIconButton(
+              icon: Icons.bookmark,
+            ),
+            _buildIconButton(
+              icon: Icons.more_horiz,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  CircleAvatar _buildAvatar(Post post) {
+    return CircleAvatar(
+      backgroundImage: CachedNetworkImageProvider(
+        post.user?.imageUrl ?? ""
+      ),
+      backgroundColor: Colors.grey[100],
+    );
+  }
+
+  Widget _buildIconButton({
+    IconData icon,
+    VoidCallback onPressed
+  }) {
+    return GestureDetector(
+        child: Icon(icon, size: 16, color: Colors.grey,),
+        onTap: onPressed,
+      )
+    ;
+  }
+}
