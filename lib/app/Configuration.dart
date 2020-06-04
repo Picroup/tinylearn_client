033e25b32ebad2dd458fefe01518da7440ec2d94
beeth0ven <beeth0vendev@gmail.com>
2020-06-04 11:43:50 +0800

    引入 dev 配置

--- lib/app/Configuration.dart
+++ lib/app/Configuration.dart
@@ -2,6 +2,11 @@
 final configuration = _dev;
 
 final _dev = Configuration(
+  uri: 'https://tinylearndev.picroup.com:444/',
+  codeCountDownSeconds: 60,
+);
+
+final _local = Configuration(
   uri: 'http://10.0.0.105:4004/',
   codeCountDownSeconds: 60,
 );
