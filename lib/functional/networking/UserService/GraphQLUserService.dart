dfb7c9321c9e2def53e7baa293c94753d2d3c9f1
beeth0ven <beeth0vendev@gmail.com>
2020-06-10 19:43:52 +0800

    提炼 UserBasicTile

--- lib/functional/networking/UserService/GraphQLUserService.dart
+++ lib/functional/networking/UserService/GraphQLUserService.dart
@@ -29,9 +29,9 @@ class GraphQLUserService extends UserService {
               user {
                 $userBasicFragment
                 hasSetUsername
-                unreadNotificationsCount
                 sum {
                   $userSumBasicFragment
+                  unreadNotificationsCount
                 } 
               }
             }
--- lib/widgets/PostListTile.dart
+++ lib/widgets/PostListTile.dart
@@ -1,13 +1,11 @@
 
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:tinylearn_client/models/Post.dart';
 import 'package:tinylearn_client/functional/foundation/int_time.dart';
-import 'package:tinylearn_client/functional/dateformat/shorttext_time.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-import 'MyCircleAvatar.dart';
+import 'UserBasicTile.dart';
 
 class PostListTile extends StatelessWidget {
 
@@ -18,35 +16,10 @@ class PostListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(width: 8),
-          MyCircleAvatar(url: post.user?.imageUrl),
-          SizedBox(width: 8),
-          Expanded(child: _buildPostBody(context, post, onTapLink),),
-          SizedBox(width: 16),
-        ],
-      ),
-    );
-  }
-
-
-  Column _buildPostBody(BuildContext context, Post post, MarkdownTapLinkCallback onTapLink) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+    return UserBasicTile(
+      user: post?.user,
+      dateTime: post?.created?.time,
       children: <Widget>[
-        Row(
-          children: <Widget>[
-            Text('@${post.user?.username ?? ""}', style: Theme.of(context).textTheme.subtitle1.copyWith(fontWeight: FontWeight.bold)),
-            SizedBox(width: 8),
-            Text('${post.created.time.shortText}', style: Theme.of(context).textTheme.subtitle2.copyWith(color: Colors.grey),)
-          ],
-        ),
-        SizedBox(height: 4),
         // Text(post.content, overflow: TextOverflow.ellipsis, maxLines: 5, style: Theme.of(context).textTheme.bodyText1),
         MarkdownBody(data: post.content, onTapLink: (url) async {
           if (await canLaunch(url)) {
