b7e8528a367d29bbba237b4b2afcc98b050061b9
beeth0ven <beeth0vendev@gmail.com>
2020-06-10 11:01:43 +0800

    优化搜索页

--- lib/pages/search/SearchPage.dart
+++ lib/pages/search/SearchPage.dart
@@ -20,45 +20,48 @@ class _SearchPageState extends State<SearchPage> {
     return ChangeNotifierProvider(
       create: (context) => SearchNotifier(context.read()),
       child: Consumer<SearchNotifier>(
-        builder: (context, value, child) => 
-        value.cursorTags?.items?.length == null
+        builder: (context, searchNotifier, child) => 
+        searchNotifier.cursorTags?.items?.length == null
           ? Center(child: CircularProgressIndicator())
-          :
-        DefaultTabController(
-          length: value.cursorTags.items.length,
-          child: Scaffold(
-            appBar: AppBar(
-              centerTitle: true,
-              title: FlatButton(
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.search),
-                    SizedBox(width: 16),
-                    Expanded(child: Text('搜索', style: Theme.of(context).textTheme.subtitle1,),)
-                  ],
+          : DefaultTabController(
+              length: searchNotifier.cursorTags.items.length,
+              child: Scaffold(
+                appBar: _buildAppBar(context, searchNotifier),
+                body: TabBarView(
+                  children: searchNotifier.cursorTags.items
+                      .map((tag) => TagPostsPage(tagName: tag.name))
+                      .toList(),
                 ),
-                onPressed: () {
-                  // showSearch
-                },
-              ),
-              bottom: TabBar(
-                isScrollable: true,
-                labelColor: Theme.of(context).accentColor,
-                indicatorSize: TabBarIndicatorSize.label,
-                unselectedLabelColor: Colors.grey,
-                // labelStyle: Theme.of(context).textTheme.headline6,
-                tabs: value.cursorTags.items
-                  .map((item) => Tab(text: item.name == '#__all' ? '#全部' : item.name))
-                  .toList(),
               ),
             ),
-            body: TabBarView(
-              children: value.cursorTags.items
-                  .map((tag) => TagPostsPage(tagName: tag.name))
-                  .toList(),
-            ),
-          ),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar(BuildContext context, SearchNotifier searchNotifier) {
+    return AppBar(
+      centerTitle: true,
+      title: FlatButton(
+        child: Row(
+          children: <Widget>[
+            Icon(Icons.search),
+            SizedBox(width: 16),
+            Expanded(child: Text('搜索', style: Theme.of(context).textTheme.subtitle1,),)
+          ],
         ),
+        onPressed: () {
+          // showSearch
+        },
+      ),
+      bottom: TabBar(
+        isScrollable: true,
+        labelColor: Theme.of(context).accentColor,
+        indicatorSize: TabBarIndicatorSize.label,
+        unselectedLabelColor: Colors.grey,
+        // labelStyle: Theme.of(context).textTheme.headline6,
+        tabs: searchNotifier.cursorTags.items
+          .map((item) => Tab(text: item.name == '#__all' ? '#全部' : item.name))
+          .toList(),
       ),
     );
   }
